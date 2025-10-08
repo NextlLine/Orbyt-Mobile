0de3564dd71245f1299d111179dc7768c2e2306b
Jairo Júnior <jairojjr2003@gmail.com>
2025-10-08 19:49:33 -0300

    chore: added IOS native tabs (IOS 26) and Android default

--- Orbyt/app/(tabs)/_layout.tsx
+++ Orbyt/app/(tabs)/_layout.tsx
@@ -1,12 +1,47 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
+import { Platform } from 'react-native';
+import { useOrbytColor } from '@/hooks/defaultColors';
+
+import { NativeTabs, Label, Icon as NativeIcon } from 'expo-router/unstable-native-tabs';
+import { Tabs } from 'expo-router';
+import RNIcon from 'react-native-vector-icons/Feather';
 import { HapticTab } from '@/components/util/haptic-tab';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import Icon from 'react-native-vector-icons/Feather';
 
 export default function TabLayout() {
+  const color = useOrbytColor('primary');
   const colorScheme = useColorScheme();
+
+  if (Platform.OS === 'ios') {
+    const renderIcon = (iosDefault: string, iosSelected: string) => {
+      return <NativeIcon sf={{ default: iosDefault, selected: iosSelected } as any} />;
+    };
+
+    return (
+      <NativeTabs>
+        <NativeTabs.Trigger name="news/index">
+          {renderIcon('house', 'house.fill')}
+          <Label>News</Label>
+        </NativeTabs.Trigger>
+
+        <NativeTabs.Trigger name="finance/index">
+          {renderIcon('chart.bar', 'chart.bar.fill')}
+          <Label>Finance</Label>
+        </NativeTabs.Trigger>
+
+        <NativeTabs.Trigger name="investment/index">
+          {renderIcon('chart.line.uptrend.xyaxis', 'chart.line.uptrend.xyaxis')}
+          <Label>Investment</Label>
+        </NativeTabs.Trigger>
+
+        <NativeTabs.Trigger name="settings/index">
+          {renderIcon('gear', 'gear.fill')}
+          <Label>Settings</Label>
+        </NativeTabs.Trigger>
+      </NativeTabs>
+    );
+  }
 
   return (
     <Tabs
@@ -14,34 +49,34 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="news/index"
         options={{
           title: 'News',
-          tabBarIcon: ({ color }) => <Icon name="cloud" size={20} color={color} />,
+          tabBarIcon: ({ color }) => <RNIcon name="home" size={20} color={color} />,
         }}
       />
       <Tabs.Screen
         name="finance/index"
         options={{
-          title: 'Fincance',
-          tabBarIcon: ({ color }) => <Icon name="bar-chart" size={20} color={color} />,
+          title: 'Finance',
+          tabBarIcon: ({ color }) => <RNIcon name="bar-chart" size={20} color={color} />,
         }}
       />
       <Tabs.Screen
         name="investment/index"
         options={{
           title: 'Investment',
-          tabBarIcon: ({ color }) => <Icon name="trending-up" size={20} color={color} />,
+          tabBarIcon: ({ color }) => <RNIcon name="trending-up" size={20} color={color} />,
         }}
       />
-       <Tabs.Screen
+      <Tabs.Screen
         name="settings/index"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => <Icon name="settings" size={20} color={color} />
-          ,
+          tabBarIcon: ({ color }) => <RNIcon name="settings" size={20} color={color} />,
         }}
       />
     </Tabs>
